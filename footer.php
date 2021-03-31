914e188e312bd69e98edb8a1e0b4da0e0805bc6b
Adis Azizan <adisazizan@gmail.com>
2021-03-31 12:08:07 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -59,7 +59,7 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<!--<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>-->
 <script src="<?php  echo get_theme_file_uri('/js/master.js'); ?>"></script>
 </body>
 </html>
