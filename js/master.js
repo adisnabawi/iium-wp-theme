93fd70373738e93cd16dc9607abd8c5a9a414bfc
Adis Nabawi <>
2021-05-04 12:10:14 +0800

    animation

--- js/master.js
+++ js/master.js
@@ -1,6 +1,12 @@
 jQuery(document).ready(function($){
-$(".hamburger").click(function(){
+$(".hamburger").click(function(e){
+  e.preventDefault();
     $(this).next(".show-menu").toggle();
+    $(".openanimate")[0].onclick();
+});
+
+$(".openanimate").click(function(e){
+  $(".openanimate")[0].onclick();
 });
 
 $("#primary").click(function() {
@@ -9,4 +15,5 @@ $("#primary").click(function() {
 $("#content").click(function() {
     $(".show-menu").hide();
 })
+
 });
